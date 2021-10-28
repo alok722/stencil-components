0c2bdc355caafe1ade80b345f61a1107a2e26d6f
Alok Raj <alokr417@gmail.com>
2021-10-28 16:04:18 +0530

    Basic SideMenu Initialized ✔️

---
+++ src/components/side-drawer/side-drawer.tsx
@@ -0,0 +1,30 @@
+import { Component, Prop, h } from '@stencil/core';
+
+@Component({
+  tag: 'uc-side-drawer',
+  styleUrl: './side-drawer.css',
+  shadow: true
+})
+export class SideDrawer {
+  @Prop({ reflect: true }) title: string = '';
+  @Prop({ reflect: true, mutable: true }) open: boolean;
+
+  onCloseDrawer() {
+    this.open = false;
+  }
+
+
+  render() {
+    return (
+      <aside>
+        <header>
+          <h1>{this.title}</h1>
+          <button onClick={this.onCloseDrawer.bind(this)}>X</button>
+        </header>
+        <main>
+          <slot />
+        </main>
+      </aside>
+    );
+  }
+}
